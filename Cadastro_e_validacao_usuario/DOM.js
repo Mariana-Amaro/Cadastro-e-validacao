85b9e687d144a5d93c6d8b597d347018a23d561a
Mariana Amaro Neves <111287183+Mariana-Amaro@users.noreply.github.com>
2024-10-04 10:19:56 -0300

    Update DOM.js

--- Cadastro_e_validacao_usuario/DOM.js
+++ Cadastro_e_validacao_usuario/DOM.js
@@ -1,3 +1,36 @@
-var opc;
-opc =confirm('Deseja continuar?');
-document.write(opc);
+function validar(){
+    if(document.UAM_aula.teu_nome.value ==""){
+        alert("Digite seu nome!");
+        document.getElementById("teu_nome").focus();
+        return(false);
+    }
+
+    if(document.UAM_aula.tua_senha.value ==""){
+        alert("Digite a senha!");
+        document.getElementById("tua_senha").focus();
+        return(false);
+    }
+
+    if(document.UAM_aula.tua_data.value ==""){
+        alert("Digite uma data válida!");
+        document.getElementById("tua_data").focus();
+        return(false);
+    }
+
+    if(document.UAM_aula.teu_Email.value ==""){
+        alert("Digite um email!");
+        document.getElementById("teu_Email").focus();
+        return(false);
+    }
+}
+
+var data = new Date();
+var dt = new Array();
+var aux =document.UAM_aula.tua_data.value;
+    dt.aux.split("/");
+
+if( ((dt[0]<1)||(dt[0])>31) || (dt[1]<1 || (dt[1]>12)) || (dt[2] < dt.getFullYear()-150) ||(dt[2] > dt.getFullYear()))  {
+    alert("Digite uma data válida");
+    document.getElementById("tua_data").focus();
+    return(false);
+}
