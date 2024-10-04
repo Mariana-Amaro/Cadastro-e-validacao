aee08f64613e5698aff841b57d426c7fc5f17ca8
Mariana Amaro Neves <111287183+Mariana-Amaro@users.noreply.github.com>
2024-10-04 10:38:14 -0300

    Update DOM.js

--- Cadastro_e_validacao_usuario/DOM.js
+++ Cadastro_e_validacao_usuario/DOM.js
@@ -29,7 +29,8 @@ var dt = new Array();
 var aux =document.UAM_aula.tua_data.value;
     dt.aux.split("/");
 
-if( ((dt[0]<1)||(dt[0])>31) || (dt[1]<1 || (dt[1]>12)) || (dt[2] < dt.getFullYear()-150) ||(dt[2] > dt.getFullYear()))  {
+if( ((dt[0] < 1)||(dt[0] > 31)) || ((dt[1] < 1)||(dt[1] > 12)) || ((dt[2]
+< dt.getFullYear()-150)||(dt[2] > dt.getFullYear())) ) {
     alert("Digite uma data válida");
     document.getElementById("tua_data").focus();
     return(false);
