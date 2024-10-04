b5de9c270390dbc8f6f65499e13c3c5cc2119341
Mariana Amaro Neves <111287183+Mariana-Amaro@users.noreply.github.com>
2024-10-04 10:49:55 -0300

    Update DOM.js

--- Cadastro_e_validacao_usuario/DOM.js
+++ Cadastro_e_validacao_usuario/DOM.js
@@ -10,22 +10,9 @@ function validar(){
         document.getElementById("tua_isenha").focus();
         return false;
     }
-
-    if(document.UAM_aula.tua_data.value ==""){
-        alert("Digite uma data válida!");
-        document.getElementById("tua_idata").focus();
-        return false;
-    }
-
-    if(document.UAM_aula.teu_email.value ==""){
-        alert("Digite um email!");
-        document.getElementById("teu_iemail").focus();
-        return false;
-    }
-}
-
-var data = new Date();
-var dt = new Array();
+    
+    var data = new Date();
+var dt = new Array();       // array para guardar a data com dt[0] sendo dia, dt[1] o mês e dt[2] o ano.
 var aux =document.UAM_aula.tua_data.value;
     dt.aux.split("/");
 
@@ -34,4 +21,13 @@ if( ((dt[0] < 1)||(dt[0] > 31)) || ((dt[1] < 1)||(dt[1] > 12)) || ((dt[2]
     alert("Digite uma data válida");
     document.getElementById("tua_data").focus();
     return(false);
+    }
+var vm =document.UAM_aula.teu_email.value();
+    if(vm.search("@")==-1){
+        alert("Digite um e-mail válido!");
+        document.getElementById("teu_iemail").focus();
+        return false;
+    }
 }
+
+
