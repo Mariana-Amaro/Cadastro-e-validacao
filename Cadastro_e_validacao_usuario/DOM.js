f7caaabb1acaf22b12a06da2e9d643cbd54d6fc1
Mariana Amaro Neves <111287183+Mariana-Amaro@users.noreply.github.com>
2024-10-04 10:30:35 -0300

    Update DOM.js

--- Cadastro_e_validacao_usuario/DOM.js
+++ Cadastro_e_validacao_usuario/DOM.js
@@ -1,26 +1,26 @@
 function validar(){
     if(document.UAM_aula.teu_nome.value ==""){
         alert("Digite seu nome!");
-        document.getElementById("teu_nome").focus();
-        return(false);
+        document.getElementById("teu_inome").focus();
+        return false;
     }
 
     if(document.UAM_aula.tua_senha.value ==""){
         alert("Digite a senha!");
-        document.getElementById("tua_senha").focus();
-        return(false);
+        document.getElementById("tua_isenha").focus();
+        return false;
     }
 
     if(document.UAM_aula.tua_data.value ==""){
         alert("Digite uma data válida!");
-        document.getElementById("tua_data").focus();
-        return(false);
+        document.getElementById("tua_idata").focus();
+        return false;
     }
 
     if(document.UAM_aula.teu_Email.value ==""){
         alert("Digite um email!");
-        document.getElementById("teu_Email").focus();
-        return(false);
+        document.getElementById("teu_iemail").focus();
+        return false;
     }
 }
 
